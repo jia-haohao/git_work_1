a52c73f8f6695868b4c5ffe62b8493b307358472
she <fangfangshe09@gmail.com>
2023-03-13 11:58:18 +0900

    じゃんけん

--- janken.rb
+++ janken.rb
@@ -6,26 +6,27 @@ class Player
     puts " 0:グ-\n 1:チョキ\n 2:パー"
     # 変数「input_hand」にプレイヤーの入力値を代入します。
     # ヒント：getsメソッドについて調べてみましょう。
-    input_hand = gets.to_i
     # 「input_hand」が「0, 1, 2」のいずれかだと繰り返し処理を終了し、それ以外（アルファベットも含む）だと繰り返し処理を継続します。
+    input_hand = gets.chomp
     while true
       # if 「input_hand」が「0, 1, 2」のいずれかの場合だった場合
       # ヒント：include?メソッドについて調べてみましょう。
-      janken = [0,1,2]
+      janken = ["0","1","2"]
       if janken.include?(input_hand)
         # 「input_hand」をそのまま返す。
         # ヒント：戻り値を返して繰り返し処理を終了させたい場合、「return」を使用します。
-        return input_hand.to_i
+        return input_hand
         # else それ以外の場合
       else
+        # (input_hand.gets.to_s || player_hand > 2)
         # プレイヤーに「0〜2」を入力させる文章を表示させる。
         # puts "0〜2の数字を入力してください。"
         puts "0〜2の数字を入力してください。"
         # puts "0:グー, 1:チョキ, 2:パー"
         puts "0:グー, 1:チョキ, 2:パー"
-        # 変数「input_hand」にプレイヤーの入力値を代入します。
-        input_hand = gets.to_i
+      # 変数「input_hand」にプレイヤーの入力値を代入します。
         # end if文のend
+        input_hand = gets.chomp
       end
     end
   end
@@ -34,7 +35,7 @@ end
 class Enemy
   def hand
     # グー、チョキ、パーの値をランダムに取得する。
-    enemy_hand = rand(3).to_i
+    enemy_hand = rand(3).to_s
   end
 end
 # プレイヤー(自分)が入力した「0~2」と、敵がランダムで生成した「0~2」をじゃんけんをさせて、その結果をコンソール上に出力するロジックを書きます。
@@ -42,8 +43,9 @@ class Janken
   def pon(player_hand, enemy_hand)
     # 変数「janken」に["グー", "チョキ", "パー"]を代入します。
     janken = ["グー", "チョキ", "パー"]
+    # binding.irb
     #「相手の手は#{相手の手}です。」と出力させます。
-    print "相手の手は#{janken[enemy_hand]}です。"
+    print "相手の手は#{janken[enemy_hand.to_i]}です。"
     # Playerクラスの戻り値とEnemyクラスの戻り値からじゃんけんするロジックを作成します。
      # Playerクラスの戻り値(player_hand)とEnemyクラスの戻り値(enemy_hand)の値が同じだった場合
     if player_hand == enemy_hand
@@ -54,7 +56,9 @@ class Janken
       # ヒント：「return」を使って戻り値を返すことができます。しかし、Rubyでは戻り値を返す場合、「return」を省略するのが一般的です。
     elsif # もしも下記の組み合わせだった場合
       # (player_hand == 0 && enemy_hand == 1) || (player_hand == 1 && enemy_hand == 2) || (player_hand == 2 && enemy_hand == 0)
-      (player_hand == 0 && enemy_hand == 1) || (player_hand == 1 && enemy_hand == 2) || (player_hand == 2 && enemy_hand == 0)
+      # player_hand.to_i
+      # enemy_hand.to_i
+      (player_hand == "0" && enemy_hand == "1") || (player_hand == "1" && enemy_hand == "2") || (player_hand == "2" && enemy_hand == "0")
       #「あなたの勝ちです」を出力します。
       puts "あなたの勝ちです。"
       false
